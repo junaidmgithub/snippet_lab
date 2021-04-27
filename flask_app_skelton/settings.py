32d5da735fd9c13ca9e1fe2c1789f19b73be9788
junaid <39518060+junaidmgithub@users.noreply.github.com>
2021-04-27 23:47:28 +0530

    Create settings.py

---
+++ flask_app_skelton/settings.py
@@ -0,0 +1,18 @@
+
+# CORS ORIGINS
+CORS_ORIGINS = '*'
+
+# DATABASE
+DATABASES = {
+    'default': {
+        'ENGINE': 'postgres',
+        'NAME': "demo_db",
+        'USER': "postgres",
+        'PASSWORD': "123456",
+        'HOST': "127.0.0.1",
+        'PORT': "5432",
+    }
+}
+
+# Email Conf
+ADMIN_EMAIL = "admin@gmail.com"
