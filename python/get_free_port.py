133198cbb86fe45ed4b788558450eac56d8c6aac
junaid <39518060+junaidmgithub@users.noreply.github.com>
2021-08-29 20:10:43 +0530

    Create get_free_port.py

---
+++ python/get_free_port.py
@@ -0,0 +1,15 @@
+import gc
+import socket
+
+
+def get_free_port() -> int:
+    """Get free port."""
+    sock = socket.socket()
+    sock.bind(('localhost', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    del sock
+    gc.collect()
+    return port
+
+print(get_free_port())
