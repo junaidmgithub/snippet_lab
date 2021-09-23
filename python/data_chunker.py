11f9bbbe900de45f146c22988b595b66fb26962e
junaid <39518060+junaidmgithub@users.noreply.github.com>
2021-09-23 19:48:42 +0530

    Create data_chunker.py

---
+++ python/data_chunker.py
@@ -0,0 +1,39 @@
+class DataChunker:
+
+    def __init__(self, chunk_size):
+        self.chunk_size = chunk_size
+        self.size = 0
+        self.bulk_data = []
+
+    def feed(self, data):
+        ret = None
+        # full chunk, send it and start new one
+        if self.bulk_data and self.size >= self.chunk_size:
+            ret = self.bulk_data
+            self.bulk_data, self.size = [], 0
+            pass
+        self.bulk_data.append(data)
+        self.size += 1
+        return ret
+
+    def flush(self):
+        ret = None
+        if self.bulk_data:
+            ret = self.bulk_data
+            self.bulk_data = []
+        return ret
+
+def chunk_data(data, chunk_size=2):
+    chunker = DataChunker(chunk_size=chunk_size)
+    for i in data:
+        ret = chunker.feed(i)
+        if ret:
+            yield ret
+    ret = chunker.flush()
+    if ret:
+        yield ret
+
+# Test
+d = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+for i in chunk_data(d, 3):
+    print(i)
