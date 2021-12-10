0db8f21cba00c66917bb6afc66a2b873c9ee1831
junaid <39518060+junaidmgithub@users.noreply.github.com>
2021-12-10 15:05:18 +0530

    Create es_connections.py

---
+++ elasticsearch/es_connections.py
@@ -0,0 +1,39 @@
+class Connections:
+
+    connection = None
+
+    def __init__(self, **configs):
+        self.connections = {}
+        self.configs = configs
+
+    def get_connection(self, alias='default'):
+        try:
+            return self.connections[alias]
+        except KeyError:
+            pass
+        return self.create_connection(alias)
+
+    def create_connection(self, alias="default"):
+        self.connections[alias] = dict(**self.configs[alias])
+        return self.connections[alias]
+
+    def add_connection(self, alias, conn):
+        self.connections[alias] = conn
+
+    def configure(self, **kwargs):
+        self.configs.update(kwargs)
+
+
+connections = Connections(default={'host':'127.0.0.1', 'port': 5600},
+                          dev={'host':'', 'port': 123},
+                          qa={})
+configure = connections.configure
+add_connection = connections.add_connection
+get_connection = connections.get_connection
+
+
+# Usage
+print(get_connection())
+print(get_connection('dev'))
+print(get_connection('dev'))
+print(get_connection('qa'))
